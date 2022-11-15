02bd3ca4cd4762af4184bb6b93bc240fc0154e92
Cleison Pereira De Sousa <cleison.pereira@ifood.com.br>
2022-11-15 10:00:40 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##kubernets local
+###run
+`docker push api-go:latest`
+`kind create cluster --name=api-go`
+`kubectl apply -f k8s `
+`jubectl get po`
--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/go-lang-api/internal/order/infra/database"
 	"github.com/go-lang-api/internal/order/usecase"
@@ -18,8 +20,8 @@ func main() {
 	}
 
 	defer db.Close()
-	r := database.NewOrderRepository(db)
-	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: r}
+	re := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: re}
 
 	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
@@ -28,19 +30,57 @@ func main() {
 	defer ch.Close()
 	out := make(chan amqp.Delivery)
 	go rabbitmq.Consume(ch, out)
+	// go kafka.Consume(ch, out)
+	// go sqs.Consume(ch, out)
 
-	for msg := range out {
+	// for msg := range out {
+	// 	var inputDTO usecase.OrderInputDTO
+	// 	err := json.Unmarshal(msg.Body, &inputDTO)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	outputDTO, err := uc.Execute(inputDTO)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(outputDTO)
+	// 	msg.Ack(false)
+
+	// }
+
+	qtdWorkers := 150
+	for i:=1; i <= qtdWorkers; i++ {
+		go worker(out, &uc, i)
+	}
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request){
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: re}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+
+	http.ListenAndServe(":8080", nil) // Chama serve HTTP, ria uma thread
+	// go http.ListenAndServe(":8080", nil)
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
+	println()
+	for msg := range deliveryMessage {
 		var inputDTO usecase.OrderInputDTO
 		err := json.Unmarshal(msg.Body, &inputDTO)
 		if err != nil {
 			panic(err)
 		}
 		outputDTO, err := uc.Execute(inputDTO)
-		if err != nil {
+		if err != nil{
 			panic(err)
 		}
-		fmt.Println(outputDTO)
+		fmt.Printf("worker %d has processed order %s\n", workerID, outputDTO.ID)
 		msg.Ack(false)
-
+		time.Sleep(1* time.Second)
 	}
 }
